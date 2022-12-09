36889de5d95d72b4318d9f588891f30895ed26ab
CharlieMickey <charles.f.mickey@gmail.com>
2022-12-09 13:52:57 -0600

    add variables

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner, tie;
 
 
 /*------------------------ Cached Element References ------------------------*/
 
+const sqrEls = document.querySelector('.board');
+
+const statusMssg = document.querySelector('#status-message');
 
 
 /*----------------------------- Event Listeners -----------------------------*/
