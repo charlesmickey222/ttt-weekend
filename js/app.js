8ec39d08a985d9756741832020762df3dadb71b3
CharlieMickey <charles.f.mickey@gmail.com>
2022-12-09 23:29:29 -0600

    add gameplay functionality

--- js/app.js
+++ js/app.js
@@ -19,19 +19,25 @@ for (let i =0; i<9; i++){
 }
 const statusMssg = document.querySelector('#status-message');
 
-
+const rstBtn = document.querySelector('#reset-button');
+const startBtn = document.querySelector('#start-button');
 /*----------------------------- Event Listeners -----------------------------*/
 
 sqrEls.forEach(sqr =>{
   sqr.addEventListener('click',function(){
     handleClick(sqr);
-    renderBoard();
+    render();
   })
 })
+rstBtn.addEventListener('click', function(){
+  reset();
+})
 
+startBtn.addEventListener('click', init());
 /*-------------------------------- Functions --------------------------------*/
 
 function init(){
+  running = true;
   initBoard();
   turn = -1;
   winner = {status:false, victor:'player 1'};
@@ -59,17 +65,73 @@ function renderMessage(){
     statusMssg.innerText = activeMssg;
   }else if(tie === true){
     statusMssg.innerText = tieMssg;
-  }else{
+    running = false;
+  }else if (winner.status === true){
     statusMssg.innerText = `${winner['victor']} ${winMssg}`;
+    running = false;
   }
 }
 
 function handleClick(evt){
-if (turn === -1){
-  evt.innerText = 'X';
-}else if (turn === 1){
-  evt.innerText = 'O';
-}
-turn*=-1;
+placePiece(evt);
+renderBoard();
+checkForWinner();
+checkTie();
+switchTurn();
 render();
+}
+
+function placePiece(element){
+  if(running){
+    if(element.innerText === ''){
+      if (turn === -1){
+      element.innerText = 'X';
+      }else if (turn === 1){
+      element.innerText = 'O';
+      }
+    }
+  }else{
+    statusMssg.innerText = 'start a new game!';
+  }
+}
+
+
+function switchTurn(){
+  if (winner.status){
+    return;
+  }else{
+    turn*=-1;
+  }
+}
+
+function checkTie(){
+  if(board.some(sqr => {return sqr.innerText ==='null'})){
+    tie = false;
+  }else if(board.every(sqr =>{return sqr !== ''})){
+    tie = true;
+  }
+}
+
+function checkForWinner(){
+  winningCombos.forEach(function(arr){
+    let temp = [board[arr[0]],board[arr[1]],board[arr[2]]];
+    if (temp.every(elm => {return elm === 'X'})||temp.every(elm => {return elm ==='O'})){
+      winner.status = true;
+      winner.victor = temp[0];
+      running = false;
+    }
+  })
+}
+
+function reset(){
+  resetBoard();
+  init();
+}
+
+function resetBoard(){
+  sqrEls.forEach(sqr =>{
+    sqr.innerText = '';
+  });
+  statusMssg.innerText = '';
+  renderBoard();
 }
